2ac92a246311bcc8ca1ce5e03143c2d5503e47d4
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-05 13:31:50 +0300

    for loop and return array of objects

--- functions.php
+++ functions.php
@@ -10,32 +10,30 @@
         $stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
         $stmt->bind_result($id_from_db, $user_id_from_db, $number_plate_from_db, $color_from_db);
         $stmt->execute();
+
+        // massiiv kus hoiame autosid
+        $array = array();
         
-        $row_nr = 0;
-        
-        
-        echo "<table border=1>";
-        echo "<tr><th>rea nr</th><th>auto nr märk</th></tr>";
         // iga rea kohta mis on ab'is teeme midagi
         while($stmt->fetch()){
-            //saime andmed kätte
-            //echo $row_nr." ".$number_plate_from_db." <br>";
+            //suvaline muutuja, kus hoiame auto andmeid 
+            //selle hetkeni kui lisame massiivi
+               
+            // tühi objekt kus hoiame väärtusi
+            $car = new StdClass();
             
-            // uus rida
-            echo "<tr>";
-            // reanr tulba
-            echo "<td>".$row_nr."</td>";
-            // auto nr tulba
-            echo "<td>".$number_plate_from_db."</td>";
-            // panete rea kinni
-            echo "</tr>";
+            $car->id = $id_from_db;
+            $car->number_plate = $number_plate_from_db; 
             
-            
-            $row_nr++;
+            //lisan massiivi (auto lisan massiivi)
+            array_push($array, $car);
+            //echo "<pre>";
+            //var_dump($array);
+            //echo "</pre>";
         }
-        echo "</table>";
         
-        
+        //saadan tagasi
+        return $array;
         
         $stmt->close();
         $mysqli->close();
--- table.php
+++ table.php
@@ -1,7 +1,18 @@
 <?php  
     require_once("functions.php");
     
-    getAllData();
+    // kõik autod objektide kujul massiivis
+    $car_array = getAllData();
 ?>
 
 <h1>Tabel</h1>
+<?php 
+    
+    // autod ükshaaval läbi käia
+    for($i = 0; $i < count($car_array); $i++){
+        
+        echo $car_array[$i]->number_plate."<br>";
+        
+    }
+    
+?>
